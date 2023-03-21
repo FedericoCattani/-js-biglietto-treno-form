7302056e504544d0a778f9e265daa93ff1cf2792
FedericoCattani <federico.cattani1@gmail.com>
2023-03-21 18:45:24 +0100

    stacked in js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-let nameUser;
-let kmUser;
+//deve stare dentro al click
+const button = document.getElementById('confirm');
+button.addEventListener('click', function(){
+
+  let nameUser = document.getElementById('nameUser').value;
+  console.log(nameUser);
+
+  let distanceUser = document.getElementById('kmUser').value;
+  console.log(distanceUser);
+
+  let ageRange = document.getElementById('ageRange').value;
+  console.log(ageRange);
+})
 
 
-let basicTicketprice = 0.21 * kmUser;
 
+let distanceUser = document.getElementById('kmUser').value;
+console.log(distanceUser);
 
+let basicTicketprice = 0.21 * distanceUser;
+console.log(basicTicketprice);
 
 let discountedTicket = (basicTicketprice * 20) / 100;
 let discountedTicketSenior = (basicTicketprice * 40) / 100;
 
 let finalPrice = basicTicketprice - discountedTicket;
 let finalPriceSenior = basicTicketprice - discountedTicketSenior;
-
-document.getElementById('nameUser').innerHTML = nameUser;
-
-
 
